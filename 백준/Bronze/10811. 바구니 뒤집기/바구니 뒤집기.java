88ca77d7d090b675b5f449971949f683fd84b520
Jong Kyu Lee <92130766+jklee3409@users.noreply.github.com>
2024-07-03 21:31:12 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 212 ms, Memory: 18544 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -1,38 +1,34 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
-public class Main {
-	public static void main(String args[]) 
-	{
-		Scanner sc = new Scanner(System.in);
-		int n, m;
-		
-		n = sc.nextInt();
-		m = sc.nextInt();
-		
-		int arr[] = new int[n];
-		
-		for(int i = 0; i < n; i++) {
-			arr[i] = i + 1;
-		}
-		
-		for(int i = 0; i < m; i++) {
-			
-			int left = sc.nextInt() - 1;
-			int right = sc.nextInt() - 1;
-			
-			while(left < right) {
-				int temp = arr[left];
-				arr[left] = arr[right];
-				arr[right] = temp;
-				left++;
-				right--;
-			}
-			
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n];
 
-		}
-		
-		for(int i = 0; i < n; i ++) {
-			System.out.print(arr[i] + " ");
-		}
-	}
+        for(int i = 0; i < n; i++){
+            arr[i] = i + 1;
+        }
+
+        for (int i = 0; i < m; i++) {
+            int left = sc.nextInt() - 1;
+            int right = sc.nextInt() - 1;
+
+            while(left < right) {
+                int tmp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = tmp;
+                left++;
+                right--;
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
 }
