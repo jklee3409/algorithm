f788e1d862c17b7589cdc9709ee3d080dac61c71
Jong Kyu Lee <92130766+jklee3409@users.noreply.github.com>
2024-07-09 16:39:11 +0900

    [Bronze V] Title: 직사각형, Time: 180 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(A * B);
+
+    }
+}
