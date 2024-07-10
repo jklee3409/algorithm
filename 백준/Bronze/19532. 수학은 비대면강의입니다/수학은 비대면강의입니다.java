6481ebb7b6496c5b47c9fd33fba0a70746cccd3c
Jong Kyu Lee <92130766+jklee3409@users.noreply.github.com>
2024-07-10 22:02:15 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 128 ms, Memory: 16000 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        int f = Integer.parseInt(st.nextToken());
+
+        int x = (c * e - b * f) / (a * e - b * d);
+        int y = (a * f - c * d) / (a * e - b * d);
+
+        System.out.println(x + " " + y);
+    }
+}
