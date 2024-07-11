84e199d2487a9d9299c383f4f8a4ea54084cd264
Jong Kyu Lee <92130766+jklee3409@users.noreply.github.com>
2024-07-11 23:20:37 +0900

    [Silver V] Title: 영화감독 숌, Time: 264 ms, Memory: 86288 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -7,7 +7,7 @@ public class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int n = 0;
+        int n = 665;
         int count = 0;
 
         while (true) {
