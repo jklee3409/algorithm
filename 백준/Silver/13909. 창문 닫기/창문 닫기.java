95afd4c0362a9752b82c83887b76e77b5f124194
Jong Kyu Lee <92130766+jklee3409@users.noreply.github.com>
2024-07-23 14:03:17 +0900

    [Silver V] Title: 창문 닫기, Time: 108 ms, Memory: 14120 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int count = 0;
+
+        for (int i = 1; i * i <= N; i++) {
+            count++;
+        }
+
+        br.close();
+        System.out.println(count);
+    }
+}
