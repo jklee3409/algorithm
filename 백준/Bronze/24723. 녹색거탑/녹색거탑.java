8a60e9f3c735672266602da261da836cb80167f7
Jong Kyu Lee <92130766+jklee3409@users.noreply.github.com>
2024-07-29 16:01:13 +0900

    [Bronze IV] Title: 녹색거탑, Time: 108 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        System.out.println((int)Math.pow(2,N));
+    }
+}
