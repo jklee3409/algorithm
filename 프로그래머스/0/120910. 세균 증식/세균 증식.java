1c50e6d0e8ad05566f007f39b731276c09cee110
Jong Kyu Lee <92130766+jklee3409@users.noreply.github.com>
2025-02-08 21:41:45 +0900

    [level 0] Title: 세균 증식, Time: 0.03 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/0/120910. 세균 증식/세균 증식.java
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.java
@@ -1,11 +1,6 @@
 class Solution {
     public int solution(int n, int t) {
-        int answer = n;
         
-        for (int i = 0; i < t; i++) {
-            answer *= 2;
-        } 
-        
-        return answer;
+        return n << t;
     }
 }
