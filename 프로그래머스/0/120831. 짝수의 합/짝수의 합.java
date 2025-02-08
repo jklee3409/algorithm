0568b28fbfbd43f0a426d7c2a63af408dc53fc5f
Jong Kyu Lee <92130766+jklee3409@users.noreply.github.com>
2025-02-08 20:54:34 +0900

    [level 0] Title: 짝수의 합, Time: 0.02 ms, Memory: 84.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        if (n < 2) return 0;
+        
+        for(int i = 2; i <= n; i += 2) {
+            answer += i;
+        }
+        
+        return answer;
+    }
+}
