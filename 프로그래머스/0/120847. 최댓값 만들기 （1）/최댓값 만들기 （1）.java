9cec2986da53e2d0e8ef77ac927b6187d5cf6efc
Jong Kyu Lee <92130766+jklee3409@users.noreply.github.com>
2025-02-08 21:32:00 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.55 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -1,14 +1,10 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] numbers) {
-        int max = Integer.MIN_VALUE;
-        int n = numbers.length;
+        Arrays.sort(numbers);
+        int len = numbers.length;
         
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = i + 1; j < n; j++) {
-                max = Math.max(max, numbers[i] * numbers[j]);
-            }
-        }
-        
-        return max == Integer.MIN_VALUE ? 0 : max;
+        return numbers[len - 1] * numbers[len - 2];
     }
 }
