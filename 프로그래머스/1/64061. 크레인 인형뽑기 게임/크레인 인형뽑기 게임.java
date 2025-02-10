bd32cf79cd718108af31c7fd94388e502442f397
Jong Kyu Lee <92130766+jklee3409@users.noreply.github.com>
2025-02-10 15:41:19 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.01 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -1,31 +1,35 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int[][] board, int[] moves) {
-        Stack<Integer> stack = new Stack<>();
-        int count = 0;
-        
-        for (int i = 0; i < moves.length; i++) {
-            int line = moves[i] - 1;
-            
-            for (int row = 0; row < board.length; row++) {
-                int doll = board[row][line];
-                
-                if(doll == 0) continue;
-                                
-                if (!stack.isEmpty() && stack.peek() == doll) {
-                    board[row][line] = 0;
-                    stack.pop();
-                    count += 2;
-                    break;
-                } else {
-                    board[row][line] = 0;
-                    stack.push(doll);
-                    break;
-                } 
+
+        public static int solution(int[][] board, int[] moves) {
+            Stack<Integer> stack = new Stack<>();
+            int removedDollsCount = 0;
+
+            for (int move : moves) {
+                int column = move - 1;
+                int pickedDoll = pickDoll(board, column);
+
+                if (pickedDoll != 0) {
+                    if (!stack.isEmpty() && stack.peek() == pickedDoll) {
+                        stack.pop();
+                        removedDollsCount += 2; // 2개가 터짐
+                    } else {
+                        stack.push(pickedDoll);
+                    }
+                }
             }
+            return removedDollsCount;
         }
-        
-        return count;
+
+        private static int pickDoll(int[][] board, int column) {
+            for (int row = 0; row < board.length; row++) {
+                if (board[row][column] != 0) {
+                    int doll = board[row][column];
+                    board[row][column] = 0; // 인형 꺼내기
+                    return doll;
+                }
+            }
+            return 0; // 꺼낼 인형이 없는 경우
+        }
     }
-}
