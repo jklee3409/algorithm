1cd6a5159cf17620c615f93ae7630a8f48cd6b76
Jong Kyu Lee <92130766+jklee3409@users.noreply.github.com>
2025-02-10 13:15:13 +0900

    [level 2] Title: 올바른 괄호, Time: 18.12 ms, Memory: 54.9 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -9,9 +9,7 @@ class Solution {
         while (idx < s.length()) {
             if (s.charAt(idx) == '(') stack.push('(');
             else {
-                if(stack.isEmpty()) return false;
-                
-                if (stack.pop() != '(') return false;
+                if(stack.isEmpty() || stack.pop() != '(') return false;
             }
             
             idx++;
