68d74c83529490b8cad95a93d414c49f8c10fbda
Jong Kyu Lee <92130766+jklee3409@users.noreply.github.com>
2025-02-11 15:51:18 +0900

    [Silver II] Title: 랜선 자르기, Time: 160 ms, Memory: 17856 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int K;
+    static int N;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        K = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        arr = new int[K];
+
+        long max = 0;
+
+        for (int i = 0; i < K; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            if (max < arr[i]) {
+                max = arr[i];
+            }
+        }
+
+        long min = 1;
+        long result = 0;  
+
+        while (min <= max) { 
+            long mid = (min + max) / 2;
+            long count = 0;
+
+            for (int i : arr) {
+                count += (i / mid);
+            }
+
+            if (count < N) {
+                max = mid - 1;  
+            } else {
+                result = mid;  
+                min = mid + 1;
+            }
+        }
+
+        System.out.println(result);  
+    }
+}
