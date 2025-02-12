4796cb4421e963a7ba4c0d54b1dccda76ee870e5
Jong Kyu Lee <92130766+jklee3409@users.noreply.github.com>
2025-02-12 23:05:45 +0900

    [Silver II] Title: 종이의 개수, Time: 820 ms, Memory: 309208 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.java
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.java
@@ -5,35 +5,40 @@ import java.util.StringTokenizer;
 
 public class Main {
     static int N;
-    static int[][] arr;
-    static int[] result = new int[3];
+    static int[][] paper;
+    static int[] result = new int[3]; // -1 -> 0, 0 -> 1, 1 -> 2
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        arr = new int[N][N];
+        paper = new int[N][N];
 
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
+                paper[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        solution(0,0, N);
+        solution(0, 0, N);
 
-        System.out.println(result[0]);
-        System.out.println(result[1]);
-        System.out.println(result[2]);
+        for (int i : result) {
+            System.out.println(i);;
+        }
     }
 
-    private static void solution(int x, int y, int size) {
-        if (check(x,y,size)) {
-            result[arr[x][y] + 1]++;
+    static void solution(int x, int y, int size) {
+        // 기저 조건
+        // 해당 영역이 모두 같은 숫자인 경우
+        if (check(x, y, size)){
+            result[paper[x][y] + 1]++;
             return;
         }
 
         int newSize = size / 3;
+
+        // 구역을 9개로 쪼갬 -> 행과 열을 3개씩 나눈다
+        // 각 구역에 대한 재귀 호출
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 solution(x + i * newSize, y + j * newSize, newSize);
@@ -41,12 +46,13 @@ public class Main {
         }
     }
 
+    // 해당 영역을 순회 하면서 모두 같은 숫자로 이루어져 있는지 확인
     static boolean check(int x, int y, int size) {
-        int first = arr[x][y];
+        int first = paper[x][y];
 
         for (int i = x; i < x + size; i++) {
             for (int j = y; j < y + size; j++) {
-                if (first != arr[i][j]) return false;
+                if (first != paper[i][j]) return false;
             }
         }
 
