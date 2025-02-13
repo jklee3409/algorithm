dcaaa232a3ff9f8b3c020414ea17784311e64a25
Jong Kyu Lee <92130766+jklee3409@users.noreply.github.com>
2025-02-13 17:22:19 +0900

    [Silver II] Title: DFS와 BFS, Time: 292 ms, Memory: 21148 KB -BaekjoonHub

---
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -0,0 +1,81 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static List<Integer>[] graph;
+    private static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken()); // 정점의 개수
+        int M = Integer.parseInt(st.nextToken()); // 간선의 개수
+        int V = Integer.parseInt(st.nextToken()); // 시작 정점
+
+        graph = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph[a].add(b);
+            graph[b].add(a);
+        }
+
+        // 정점 번호가 작은 것을 먼저 방문 하기 위해 
+        for (int i = 1; i <= N; i++) {
+            Collections.sort(graph[i]);
+        }
+
+        // DFS 
+        visited = new boolean[N + 1];
+        dfs(V);
+        System.out.println();
+
+        // BFS 
+        visited = new boolean[N + 1];
+        bfs(V);
+    }
+
+    private static void dfs(int node) {
+        visited[node] = true;
+        System.out.print(node + " ");
+
+        for (int next : graph[node]) {
+            if (!visited[next]) {
+                dfs(next);
+            }
+        }
+    }
+
+    private static void bfs(int start) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(start);
+        visited[start] = true;
+
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            System.out.print(node + " ");
+
+            for (int next : graph[node]) {
+                if (!visited[next]) {
+                    visited[next] = true;
+                    queue.offer(next);
+                }
+            }
+        }
+        System.out.println();
+    }
+}
