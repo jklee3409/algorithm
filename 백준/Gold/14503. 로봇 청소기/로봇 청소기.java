b9a5228ea84e0c3f194fc296b76a11be21f3c487
Jong Kyu Lee <92130766+jklee3409@users.noreply.github.com>
2025-02-18 16:58:22 +0900

    [Gold V] Title: 로봇 청소기, Time: 108 ms, Memory: 14396 KB -BaekjoonHub

---
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -0,0 +1,86 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static int r, c, d;
+    static int[][] room;
+    static boolean[][] clean;
+    //                 북  동 남  서
+    static int[] dy = {-1, 0, 1, 0};
+    static int[] dx = {0, 1, 0, -1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken()); // 행
+        M = Integer.parseInt(st.nextToken()); // 열
+        room = new int[N][M];
+        clean = new boolean[N][M];
+
+        st = new StringTokenizer(br.readLine());
+        // 로봇 청소기 위치
+        r = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+        // 로봇 청소기 초기 방향
+        d = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                room[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int cleanRoomCnt = 0;
+        while (true) {
+            // 1. 현재 칸이 청소되지 않은 경우, 청소한다.
+            if (!clean[r][c]) {
+                clean[r][c] = true;
+                cleanRoomCnt++;
+            }
+
+            // 2. 주변 4칸 중 청소되지 않은 빈 칸이 있는지 확인
+            boolean noCleanableSpace = true;
+            for (int i = 0; i < 4; i++) {
+                int ny = r + dy[i];
+                int nx = c + dx[i];
+                if (ny >= 0 && nx >= 0 && ny < N && nx < M && room[ny][nx] == 0 && !clean[ny][nx]) {
+                    noCleanableSpace = false;
+                    break;
+                }
+            }
+
+            // 2-1. 청소할 곳이 없다면 후진 시도
+            if (noCleanableSpace) {
+                int backDir = (d + 2) % 4;
+                int ny = r + dy[backDir];
+                int nx = c + dx[backDir];
+
+                // 후진 가능하면 후진
+                if (ny >= 0 && nx >= 0 && ny < N && nx < M && room[ny][nx] == 0) {
+                    r = ny;
+                    c = nx;
+                    continue;
+                } else { // 후진 불가능하면 종료
+                    break;
+                }
+            }
+
+            // 3. 청소할 곳이 있다면 반시계 방향으로 회전 후 탐색
+            d = (d + 3) % 4; // 반시계 방향으로 90도 회전
+
+            int ny = r + dy[d];
+            int nx = c + dx[d];
+
+            // 앞쪽 칸이 청소되지 않은 빈 칸이라면 전진
+            if (ny >= 0 && nx >= 0 && ny < N && nx < M && room[ny][nx] == 0 && !clean[ny][nx]) {
+                r = ny;
+                c = nx;
+            }
+        }
+
+        System.out.println(cleanRoomCnt);
+    }
+}
