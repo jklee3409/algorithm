dc24fc2114a171e26aad4e55ab59a97254008dcc
Jong Kyu Lee <92130766+jklee3409@users.noreply.github.com>
2025-02-19 17:56:41 +0900

    [Silver III] Title: 2×n 타일링, Time: 108 ms, Memory: 14364 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        if (n == 1) {
+            System.out.println(1);
+            return;
+        } else if (n == 2) {
+            System.out.println(2);
+            return;
+        }
+
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+        }
+
+        System.out.println(dp[n]);
+    }
+}
