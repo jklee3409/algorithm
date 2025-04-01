040a868b703d807c94331ff85e4f412aae5fbd0f
Jong Kyu Lee <92130766+jklee3409@users.noreply.github.com>
2025-04-01 23:01:03 +0900

    [Silver V] Title: 돌 게임, Time: 108 ms, Memory: 14316 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        System.out.println(N % 2 != 0 ? "SK" : "CY");
+    }
+}
