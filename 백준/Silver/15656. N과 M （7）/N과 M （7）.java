5a5edb2bd72330fc78c3b5ffdde20cc4f2075217
Jong Kyu Lee <92130766+jklee3409@users.noreply.github.com>
2025-04-24 14:22:27 +0900

    [Silver III] Title: N과 M (7), Time: 576 ms, Memory: 123812 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int[] input, target;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        input = new int[N];
+        target = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            input[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(input);
+        solution(0);
+        System.out.println(sb);
+
+        br.close();
+    }
+
+    public static void solution(int depth) {
+        // base camp
+        if (depth == M) {
+            for (int i : target) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            target[depth] = input[i];
+
+            solution(depth + 1);
+        }
+    }
+}
