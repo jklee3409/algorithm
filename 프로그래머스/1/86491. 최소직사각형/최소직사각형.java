b4314b2f45d7faa38d4969ef93862827891a1bdf
Jong Kyu Lee <92130766+jklee3409@users.noreply.github.com>
2025-04-24 13:14:00 +0900

    [level 1] Title: 최소직사각형, Time: 2.23 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -2,22 +2,13 @@ import java.io.*;
 
 class Solution {
     public int solution(int[][] sizes) {
-        for (int i = 0; i < sizes.length; i++) {
-            int w = sizes[i][0];
-            int h = sizes[i][1];
-
-            if (w < h) {
-                sizes[i][0] = h;    
-                sizes[i][1] = w;    
-            }
-        }
         
-        int wMAx = 0, hMAx = 0;
+        int wMax = 0, hMax = 0;
         for (int[] size : sizes) {
-            wMAx = Math.max(wMAx, size[0]);
-            hMAx = Math.max(hMAx, size[1]);
+            wMax = Math.max(wMax, Math.max(size[0], size[1]));
+            hMax = Math.max(hMax, Math.min(size[0], size[1]));
         }
 
-        return wMAx * hMAx;
+        return wMax * hMax;
     }
 }
