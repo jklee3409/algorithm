750fefbd75f7c074142ca6d1d09d23d2c8ce322e
Jong Kyu Lee <92130766+jklee3409@users.noreply.github.com>
2025-09-01 22:25:14 +0900

    [Silver I] Title: 스티커, Time: 620 ms, Memory: 110988 KB -BaekjoonHub

---
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        while (T-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            int[][] sticker = new int[2][n + 1];
+            int[][] dp = new int[2][n + 1];
+
+            for (int i = 0; i < 2; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                for (int j = 1; j < n + 1; j++) {
+                    sticker[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            dp[0][1] = sticker[0][1];
+            dp[1][1] = sticker[1][1];
+
+            // 해당 열에서 스티커를 떼지 않을 수도 있음.
+            for (int i = 2; i <= n; i++) {
+                dp[0][i] = Math.max(dp[1][i-1], dp[1][i-2]) + sticker[0][i];
+                dp[1][i] = Math.max(dp[0][i-1], dp[0][i-2]) + sticker[1][i];
+            }
+
+            System.out.println(Math.max(dp[0][n], dp[1][n]));
+        }
+    }
+}
