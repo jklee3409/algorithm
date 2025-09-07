1725ab647b591c0a81abf25bba78d96f10a69569
Jong Kyu Lee <92130766+jklee3409@users.noreply.github.com>
2025-09-07 14:49:48 +0900

    [Gold IV] Title: 테트로미노, Time: 640 ms, Memory: 33920 KB -BaekjoonHub

---
+++ 백준/Gold/14500. 테트로미노/테트로미노.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int N, M;
+    static int[][] paper;
+    static boolean[][] visited;
+    static int max = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        paper = new int[N][M];
+        visited = new boolean[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                paper[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                visited[i][j] = true;
+                dfs(i, j, paper[i][j], 1);
+                visited[i][j] = false;
+            }
+        }
+
+        System.out.println(max);
+    }
+
+    static int[] dy = {-1, 1, 0, 0};
+    static int[] dx = {0, 0, -1, 1};
+
+    static void dfs(int y, int x, int sum, int depth) {
+        if (depth == 4) {
+            max = Math.max(max, sum);
+            return;
+        }
+
+        for (int d = 0; d < 4; d++) {
+            int ny = y + dy[d];
+            int nx = x + dx[d];
+
+            if (ny < 0 || nx < 0 || ny >= N || nx >= M) continue;
+            if (visited[ny][nx]) continue;
+
+            if (depth == 2) {
+                visited[ny][nx] = true;
+                dfs(y, x, sum + paper[ny][nx], depth + 1);
+                visited[ny][nx] = false;
+            }
+
+            visited[ny][nx] = true;
+            dfs(ny, nx, sum + paper[ny][nx], depth + 1);
+            visited[ny][nx] = false;
+        }
+    }
+}
