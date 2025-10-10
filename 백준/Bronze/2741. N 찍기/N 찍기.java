f4cc75d97ae8cc3efe3dcba3ca19853870f4a373
Jong Kyu Lee <92130766+jklee3409@users.noreply.github.com>
2025-10-10 15:40:46 +0900

    [Bronze V] Title: N 찍기, Time: 188 ms, Memory: 18048 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            sb.append(i).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
