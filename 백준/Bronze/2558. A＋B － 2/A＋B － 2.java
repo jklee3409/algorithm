5da0090441eb39a8a2d40c91a62077ce25898995
Jong Kyu Lee <92130766+jklee3409@users.noreply.github.com>
2025-11-08 13:15:42 +0900

    [Bronze V] Title: A+B - 2, Time: 100 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+
+        System.out.println(A + B);
+    }
+}
