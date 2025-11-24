fa02746bd8e88bf5bea002bd6c6de7aa044fe483
Jong Kyu Lee <92130766+jklee3409@users.noreply.github.com>
2025-11-24 20:32:33 +0900

    [Silver I] Title: 경로 찾기, Time: 164 ms, Memory: 16804 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -9,50 +9,39 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+
         N = Integer.parseInt(br.readLine());
-        for (int i = 0; i < N + 1; i++) graph.add(new ArrayList<>());
+        for (int i = 0; i < N; i++) graph.add(new ArrayList<>());
         result = new int[N][N];
 
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
                 int input = Integer.parseInt(st.nextToken());
-
-                if (i == j) continue;
                 if (input == 1) graph.get(i).add(j);
             }
         }
 
         for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                visited = new boolean[N];
-                dfs(i, j, i, 1);
-            }
+            visited = new boolean[N];
+            dfs(i, i);
         }
 
         StringBuilder sb = new StringBuilder();
         for (int[] arr : result) {
-            for (int i : arr) {
-                sb.append(i).append(" ");
-            }
+            for (int i : arr) sb.append(i).append(" ");
             sb.append("\n");
         }
-
-        System.out.println(sb);
+        System.out.print(sb);
     }
 
-    static void dfs(int src, int dst, int node, int depth) {
-        if (depth != 1) visited[node] = true;
-        
-        if (dst == node && depth != 1) {
-            result[src][dst] = 1;
-            return;
-        }
+    static void dfs(int src, int node) {
+        for (int next : graph.get(node)) {
+            if (visited[next])  continue;
 
-        for (Integer next : graph.get(node)) {
-            if (visited[next]) continue;
-            dfs(src, dst, next, depth + 1);
+            visited[next] = true;
+            result[src][next] = 1;  
+            dfs(src, next);
         }
     }
 }
