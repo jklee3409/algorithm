a03fe670471aa4c778202995c109f1b7a8d61d30
Jong Kyu Lee <92130766+jklee3409@users.noreply.github.com>
2025-11-28 16:08:10 +0900

    [Gold V] Title: 동전 2, Time: 116 ms, Memory: 14276 KB -BaekjoonHub

---
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] coins = new int[n];
+        for (int i = 0; i < n; i++) coins[i] = Integer.parseInt(br.readLine());
+
+        int INF = 1000000000;
+        long[] dp = new long[k + 1];
+        Arrays.fill(dp, INF);
+        dp[0] = 0;
+
+        for (int coin : coins) {
+            for (int i = coin; i <= k; i++) {
+                dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+            }
+        }
+
+        System.out.println(dp[k] == INF ? -1 : dp[k]);
+    }
+}
