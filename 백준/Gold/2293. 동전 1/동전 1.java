d96cd0ba7fd86e59ef1674fafa1853c0c854122e
Jong Kyu Lee <92130766+jklee3409@users.noreply.github.com>
2025-11-28 15:39:03 +0900

    [Gold IV] Title: 동전 1, Time: 116 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] coins = new int[n];
+        for (int i = 0; i < n; i++) coins[i] = Integer.parseInt(br.readLine());
+
+        long[] dp = new long[k + 1];
+        dp[0] = 1; 
+
+        for (int coin : coins) {
+            for (int i = coin; i <= k; i++) {
+                dp[i] += dp[i - coin];
+            }
+        }
+
+        System.out.println(dp[k]);
+    }
+}
