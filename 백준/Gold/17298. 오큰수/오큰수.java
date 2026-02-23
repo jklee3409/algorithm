e833089e962c82c3fac02e1074273bb65219d3cf
Jong Kyu Lee <92130766+jklee3409@users.noreply.github.com>
2026-02-23 16:26:36 +0900

    [Gold IV] Title: 오큰수, Time: 912 ms, Memory: 142580 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        int[] result = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
+        
+        Stack<Integer> stack = new Stack<>();
+        for (int i = N - 1; i >= 0; i--) {
+
+            while (!stack.isEmpty() && stack.peek() <= arr[i]) {
+                stack.pop();
+            }
+
+            if (stack.isEmpty()) {
+                result[i] = -1;
+            } else {
+                result[i] = stack.peek();
+            }
+
+            stack.push(arr[i]);
+        }
+
+        for (int i = 0; i < N; i++) {
+            sb.append(result[i]).append(" ");
+        }
+
+        System.out.println(sb);
+    }
+}
