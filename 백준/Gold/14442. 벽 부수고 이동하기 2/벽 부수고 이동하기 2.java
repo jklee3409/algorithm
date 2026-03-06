d7e520395f25b07d8bdf1524a8d1d8bb84355f4a
Jong Kyu Lee <92130766+jklee3409@users.noreply.github.com>
2026-03-06 10:57:16 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 1404 ms, Memory: 338508 KB -BaekjoonHub

--- 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
@@ -2,24 +2,19 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
     static int N, M, K;
     static int[][] map;
-    static boolean[][][] visited;
 
     static class Node {
         int y, x, dist, broken;
 
-        Node(int y, int x, int dist, int broken) {
+        public Node(int y, int x, int dist, int broken) {
             this.y = y;
             this.x = x;
             this.dist = dist;
             this.broken = broken;
         }
     }
-
-    static int[] dy = {1, -1, 0, 0};
-    static int[] dx = {0, 0, 1, -1};
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -28,48 +23,51 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-
         map = new int[N][M];
-        visited = new boolean[N][M][K + 1];
 
         for (int i = 0; i < N; i++) {
-            String line = br.readLine();
+            String input = br.readLine();
             for (int j = 0; j < M; j++) {
-                map[i][j] = line.charAt(j) - '0';
+                int num = input.charAt(j) - '0';
+                map[i][j] = num;
             }
         }
 
         System.out.println(bfs());
     }
 
-    static int bfs() {
-        Queue<Node> q = new LinkedList<>();
-        q.offer(new Node(0, 0, 1, 0));
+    static int[] dy = {-1, 1, 0, 0};
+    static int[] dx = {0, 0, -1, 1};
+
+    private static int bfs() {
+        Queue<Node> queue = new ArrayDeque<>();
+        boolean[][][] visited = new boolean[N][M][K + 1];
+
+        queue.offer(new Node(0, 0, 0, 0));
         visited[0][0][0] = true;
 
-        while (!q.isEmpty()) {
-            Node cur = q.poll();
+        while (!queue.isEmpty()) {
+            Node n = queue.poll();
 
-            if (cur.y == N - 1 && cur.x == M - 1) {
-                return cur.dist;
-            }
+            if (n.y == N - 1 && n.x == M - 1) return n.dist + 1;
 
-            for (int d = 0; d < 4; d++) {
-                int ny = cur.y + dy[d];
-                int nx = cur.x + dx[d];
+            for (int i = 0; i < 4; i++) {
+                int ny = n.y + dy[i];
+                int nx = n.x + dx[i];
 
                 if (ny < 0 || nx < 0 || ny >= N || nx >= M) continue;
 
-                // 빈 칸
-                if (map[ny][nx] == 0 && !visited[ny][nx][cur.broken]) {
-                    visited[ny][nx][cur.broken] = true;
-                    q.offer(new Node(ny, nx, cur.dist + 1, cur.broken));
-                }
+                if (map[ny][nx] == 1) {
+                    if (n.broken == K) continue;
+                    if (visited[ny][nx][n.broken + 1]) continue;
 
-                // 벽
-                if (map[ny][nx] == 1 && cur.broken < K && !visited[ny][nx][cur.broken + 1]) {
-                    visited[ny][nx][cur.broken + 1] = true;
-                    q.offer(new Node(ny, nx, cur.dist + 1, cur.broken + 1));
+                    queue.offer(new Node(ny, nx, n.dist + 1, n.broken + 1));
+                    visited[ny][nx][n.broken + 1] = true;
+                } else {
+                    if (visited[ny][nx][n.broken]) continue;
+
+                    queue.offer(new Node(ny, nx, n.dist + 1, n.broken));
+                    visited[ny][nx][n.broken] = true;
                 }
             }
         }
