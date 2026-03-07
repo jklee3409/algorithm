9c2263c084a36f3609a5d6256cfe316c3ca19282
Jong Kyu Lee <92130766+jklee3409@users.noreply.github.com>
2026-03-07 22:46:15 +0900

    [Gold IV] Title: 사이클 게임, Time: 576 ms, Memory: 124832 KB -BaekjoonHub

---
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[] parent;
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        parent = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
+        }
+
+        int turn = 1;
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if (find(a) == find(b)) {
+                System.out.println(turn);
+                return;
+            }
+
+            union(a, b);
+
+            turn++;
+        }
+
+        System.out.println(0);
+    }
+
+    private static int find(int x) {
+        if (parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    private static void union(int a, int b) {
+        int pa = find(a);
+        int pb = find(b);
+
+        if (pa != pb) parent[pb] = pa;
+    }
+}
