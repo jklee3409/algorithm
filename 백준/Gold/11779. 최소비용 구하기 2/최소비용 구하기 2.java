f93a026f6d5b45209891edb1c0fa863eb0b2eb92
Jong Kyu Lee <92130766+jklee3409@users.noreply.github.com>
2026-03-11 20:12:35 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 504 ms, Memory: 51196 KB -BaekjoonHub

---
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.java
@@ -0,0 +1,111 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int[] dist;
+    static int[] prev;
+    static List<Edge>[] graph;
+    static Stack<Integer> stack = new Stack<>();
+
+    static class Edge{
+        int to, cost;
+
+        public Edge(int to, int cost){
+            this.to = to;
+            this.cost = cost;
+        }
+    }
+
+    static class State implements Comparable<State>{
+        int node, dist;
+
+        public State(int node, int dist) {
+            this.node = node;
+            this.dist = dist;
+        }
+
+        @Override
+        public int compareTo(State o) {
+            return Integer.compare(this.dist, o.dist);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        dist = new int[n + 1];
+        prev = new int[n + 1];
+        graph = new ArrayList[n + 1];
+
+        Arrays.fill(dist, Integer.MAX_VALUE);
+
+        for (int i = 1; i <= n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+
+            graph[from].add(new Edge(to, cost));
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int start = Integer.parseInt(st.nextToken());
+        int destination = Integer.parseInt(st.nextToken());
+
+        dijkstra(start);
+
+        backtracking(start, destination);
+
+        sb.append(dist[destination]).append("\n").append(stack.size()).append("\n");
+
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop()).append(" ");
+        }
+
+        System.out.println(sb);
+    }
+
+    private static void dijkstra(int start) {
+        PriorityQueue<State> pq = new PriorityQueue<>();
+
+        pq.offer(new State(start, 0));
+        dist[start] = 0;
+
+        while (!pq.isEmpty()) {
+            State cur = pq.poll();
+
+            if (dist[cur.node] < cur.dist) continue;
+
+            for (Edge next : graph[cur.node]) {
+
+                if (dist[next.to] > dist[cur.node] + next.cost) {
+                    dist[next.to] = dist[cur.node] + next.cost;
+                    prev[next.to] = cur.node;
+                    pq.offer(new State(next.to, dist[next.to]));
+                }
+            }
+        }
+    }
+
+    private static void backtracking(int start, int destination) {
+        int cur = destination;
+
+        stack.push(cur);
+
+        while (cur != start) {
+            stack.push(prev[cur]);
+            cur = prev[cur];
+        }
+    }
+}
