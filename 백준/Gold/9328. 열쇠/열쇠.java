9c6b29bc8e3b6833cf4742903fdcd2b1604d609c
Jong Kyu Lee <92130766+jklee3409@users.noreply.github.com>
2026-03-11 14:07:56 +0900

    [Gold I] Title: 열쇠, Time: 216 ms, Memory: 20048 KB -BaekjoonHub

--- 백준/Gold/9328. 열쇠/열쇠.java
+++ 백준/Gold/9328. 열쇠/열쇠.java
@@ -3,22 +3,23 @@ import java.util.*;
 
 public class Main {
 
-    static int N, M;
+    static int T, N, M;
     static char[][] map;
+    static Set<Character> keySet = new HashSet<>();
 
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        int T = Integer.parseInt(br.readLine());
+        T = Integer.parseInt(br.readLine());
 
         while (T-- > 0) {
             StringTokenizer st = new StringTokenizer(br.readLine());
 
             N = Integer.parseInt(st.nextToken());
             M = Integer.parseInt(st.nextToken());
-
             map = new char[N + 2][M + 2];
+
             for (int i = 0; i < N + 2; i++) {
                 Arrays.fill(map[i], '.');
             }
@@ -30,7 +31,7 @@ public class Main {
                 }
             }
 
-            Set<Character> keySet = new HashSet<>();
+            keySet = new HashSet<>();
 
             String readLine = br.readLine();
             if (readLine.charAt(0) != '0') {
@@ -39,7 +40,7 @@ public class Main {
                 }
             }
 
-            sb.append(bfs(keySet)).append("\n");
+            sb.append(bfs()).append("\n");
         }
 
         System.out.println(sb);
@@ -48,18 +49,18 @@ public class Main {
     static int[] dy = {-1, 1, 0, 0};
     static int[] dx = {0, 0, -1, 1};
 
-    private static int bfs(Set<Character> keySet) {
+    private static int bfs() {
         int result = 0;
 
         Queue<int[]> queue = new ArrayDeque<>();
         boolean[][] visited = new boolean[N + 2][M + 2];
-        List<int[]>[] doorList = new ArrayList[26];
+        List<int[]>[] doors = new ArrayList[26];
 
         for (int i = 0; i < 26; i++) {
-            doorList[i] = new ArrayList<>();
+            doors[i] = new ArrayList<>();
         }
 
-        queue.offer(new int[]{0, 0});
+        queue.add(new int[]{0, 0});
         visited[0][0] = true;
 
         while (!queue.isEmpty()) {
@@ -75,35 +76,37 @@ public class Main {
                 char now = map[ny][nx];
 
                 if (now == '.') {
-                    visited[ny][nx] = true;
                     queue.offer(new int[]{ny, nx});
+                    visited[ny][nx] = true;
                 }
                 else if (now == '$') {
-                    visited[ny][nx] = true;
-                    queue.offer(new int[]{ny, nx});
                     map[ny][nx] = '.';
                     result++;
+
+                    queue.offer(new int[]{ny, nx});
+                    visited[ny][nx] = true;
                 }
                 else if (Character.isUpperCase(now)) {
                     int idx = now - 'A';
 
                     if (keySet.contains(Character.toLowerCase(now))) {
-                        visited[ny][nx] = true;
                         queue.offer(new int[]{ny, nx});
-                    } else {
                         visited[ny][nx] = true;
-                        doorList[idx].add(new int[]{ny, nx});
+                    }
+                    else {
+                        doors[idx].add(new int[]{ny, nx});
+                        visited[ny][nx] = true;
                     }
                 }
                 else if (Character.isLowerCase(now)) {
-                    visited[ny][nx] = true;
                     queue.offer(new int[]{ny, nx});
+                    visited[ny][nx] = true;
 
                     if (!keySet.contains(now)) {
                         keySet.add(now);
 
                         int idx = now - 'a';
-                        for (int[] door : doorList[idx]) {
+                        for (int[] door : doors[idx]) {
                             queue.offer(new int[]{door[0], door[1]});
                         }
                     }
