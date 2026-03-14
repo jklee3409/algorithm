f217e3fa914ff9abd0636416987fbdf51b6e2948
Jong Kyu Lee <92130766+jklee3409@users.noreply.github.com>
2026-03-14 14:48:10 +0900

    [Gold V] Title: 암호코드, Time: 104 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Gold/2011. 암호코드/암호코드.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int MILLION = 1000000;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+
+        int len = input.length();
+        int[] arr = new int[len + 1];
+        int[] dp = new int[len + 1];
+
+        for (int i = 1; i <= len; i++) {
+            arr[i] = input.charAt(i - 1) - '0';
+        }
+
+        if (arr[1] == 0) {
+            System.out.println(0);
+            return;
+        }
+
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= len; i++) {
+            int one = arr[i];
+            int two = arr[i - 1] * 10 + arr[i];
+
+            if (1 <= one && one <= 9) {
+                dp[i] = (dp[i] + dp[i - 1]) % MILLION;
+            }
+
+            if (two >= 10 && two <= 26) {
+                dp[i] = (dp[i] + dp[i - 2]) % MILLION;
+            }
+        }
+
+        System.out.println(dp[len]);
+    }
+}
