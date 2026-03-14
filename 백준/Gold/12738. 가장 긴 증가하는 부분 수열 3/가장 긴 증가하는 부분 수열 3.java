a7156273934f65aece86acee8f79fe91fabfdc1c
Jong Kyu Lee <92130766+jklee3409@users.noreply.github.com>
2026-03-14 19:13:38 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 516 ms, Memory: 121708 KB -BaekjoonHub

---
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] lis = new int[N];
+        int len = 0;
+
+        for (int i = 0; i < N; i++) {
+            int pos = lowerBound(lis, 0, len, arr[i]);
+            lis[pos] = arr[i];
+
+            if (pos == len) len++;
+        }
+
+        System.out.println(len);
+    }
+
+    private static int lowerBound(int[] arr, int left, int right, int target) {
+        while (left < right) {
+            int mid = (left + right) / 2;
+
+            if (target <= arr[mid]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+}
