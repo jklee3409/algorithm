b996e4f0ba50d4c142abf608789c0fab069fa4f5
Jong Kyu Lee <92130766+jklee3409@users.noreply.github.com>
2026-03-16 20:37:51 +0900

    [Gold II] Title: 계보 복원가 호석, Time: 672 ms, Memory: 134076 KB, Score: 19 point -BaekjoonHub

--- 백준/Gold/21276. 계보 복원가 호석/계보 복원가 호석.java
+++ 백준/Gold/21276. 계보 복원가 호석/계보 복원가 호석.java
@@ -10,78 +10,69 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
 
-        Map<String, Integer> map = new HashMap<>();
         String[] people = new String[N];
-        int[] indegree = new int[N + 1];
-
-        List<Integer>[] graph = new ArrayList[N + 1];
-        for (int i = 1; i <= N; i++) {
-            graph[i] = new ArrayList<>();
-        }
-
+        
         st = new StringTokenizer(br.readLine());
-
+        
         for (int i = 0; i < N; i++) {
             people[i] = st.nextToken();
         }
 
         Arrays.sort(people);
 
+        Map<String, Integer> map = new HashMap<>();
         for (int i = 0; i < N; i++) {
             map.put(people[i], i + 1);
         }
 
-        int M = Integer.parseInt(br.readLine());
+        int[] indegree = new int[N + 1];
+        List<Integer>[] graph = new ArrayList[N + 1];
+        List<Integer>[] childList = new ArrayList[N + 1];
 
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+            childList[i] = new ArrayList<>();
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
 
-            String b = st.nextToken();
-            String a = st.nextToken();
+            String child = st.nextToken();
+            String parent = st.nextToken();
 
-            int A = map.get(a);
-            int B = map.get(b);
+            int p = map.get(parent);
+            int c = map.get(child);
 
-            graph[A].add(B);
-            indegree[B]++;
+            graph[p].add(c);
+            indegree[c]++;
         }
 
         Queue<Integer> q = new ArrayDeque<>();
-
-        int fatherCnt = 0;
-        List<String> faterList = new ArrayList<>();
+        List<Integer> roots = new ArrayList<>();
 
         for (int i = 1; i <= N; i++) {
             if (indegree[i] == 0) {
-                fatherCnt++;
-                faterList.add(people[i - 1]);
+                roots.add(i);
                 q.offer(i);
             }
         }
 
-        sb.append(fatherCnt).append("\n");
-
-        Collections.sort(faterList);
-
-        for (String father : faterList) {
-            sb.append(father).append(" ");
+        sb.append(roots.size()).append('\n');
+        
+        for (int root : roots) {
+            sb.append(people[root - 1]).append(' ');
         }
-
-        sb.append("\n");
-
-        List<String>[] childList = new ArrayList[N + 1];
-        for (int i = 1; i <= N; i++) {
-            childList[i] = new ArrayList<>();
-        }
+        
+        sb.append('\n');
 
         while (!q.isEmpty()) {
-            Integer cur = q.poll();
+            int cur = q.poll();
 
-            for (Integer next : graph[cur]) {
-
+            for (int next : graph[cur]) {
                 if (indegree[next] == 1) {
-                    String child = people[next - 1];
-                    childList[cur].add(child);
+                    childList[cur].add(next);
                 }
 
                 if (--indegree[next] == 0) {
@@ -93,15 +84,15 @@ public class Main {
         for (int i = 1; i <= N; i++) {
             Collections.sort(childList[i]);
 
-            sb.append(people[i - 1]).append(" ").append(childList[i].size()).append(" ");
-
-            for (String child : childList[i]) {
-                sb.append(child).append(" ");
+            sb.append(people[i - 1]).append(' ').append(childList[i].size()).append(' ');
+            
+            for (int child : childList[i]) {
+                sb.append(people[child - 1]).append(' ');
             }
-
-            sb.append("\n");
+            
+            sb.append('\n');
         }
 
-        System.out.println(sb);
+        System.out.print(sb);
     }
 }
