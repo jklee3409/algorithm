2c07bd191cd7dd546542c5a36f61a30b727fc125
Jong Kyu Lee <92130766+jklee3409@users.noreply.github.com>
2026-03-16 19:31:09 +0900

    [Gold I] Title: 최종 순위, Time: 384 ms, Memory: 48356 KB -BaekjoonHub

---
+++ 백준/Gold/3665. 최종 순위/최종 순위.java
@@ -0,0 +1,106 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+
+        while (T-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+
+            int[] lastYear = new int[n];
+            int[] indegree = new int[n + 1];
+            boolean[][] graph = new boolean[n + 1][n + 1];
+
+            st = new StringTokenizer(br.readLine());
+
+            for (int i = 0; i < n; i++) {
+                lastYear[i] = Integer.parseInt(st.nextToken());
+            }
+
+            for (int i = 0; i < n - 1; i++) {
+                for (int j = i + 1; j < n; j++) {
+                    int higher = lastYear[i];
+                    int lower = lastYear[j];
+
+                    if (!graph[higher][lower]) {
+                        graph[higher][lower] = true;
+                        indegree[lower]++;
+                    }
+                }
+            }
+
+            int m = Integer.parseInt(br.readLine());
+
+            for (int i = 0; i < m; i++) {
+                st = new StringTokenizer(br.readLine());
+
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+
+                if (graph[a][b]) {
+                    graph[a][b] = false;
+                    graph[b][a] = true;
+                    indegree[b]--;
+                    indegree[a]++;
+                } else {
+                    graph[b][a] = false;
+                    graph[a][b] = true;
+                    indegree[a]--;
+                    indegree[b]++;
+                }
+            }
+
+            Queue<Integer> q = new ArrayDeque<>();
+
+            for (int i = 1; i <= n; i++) {
+                if (indegree[i] == 0) q.offer(i);
+            }
+
+            List<Integer> result = new ArrayList<>();
+            boolean impossible = false;
+            boolean uncertain = false;
+
+            for (int i = 0; i < n; i++) {
+                if (q.isEmpty()) {
+                    impossible = true;
+                    break;
+                }
+
+                if (q.size() > 1) { // 사이클이 생길 수도 있으므로 (impossible) break X
+                    uncertain = true;
+                }
+
+                int cur = q.poll();
+                result.add(cur);
+
+                for (int next = 1; next <= n; next++) {
+                    
+                    if (graph[cur][next]) {
+                        
+                        if (--indegree[next] == 0) {
+                            q.offer(next);
+                        }
+                    }
+                }
+            }
+
+            if (impossible) sb.append("IMPOSSIBLE");
+            else if (uncertain) sb.append("?");
+            else {
+                for (Integer i : result) {
+                    sb.append(i).append(" ");
+                }
+            }
+
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
