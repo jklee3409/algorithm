1e454e9aa68ecaa1249077c709e592d5a0e95da5
Jong Kyu Lee <92130766+jklee3409@users.noreply.github.com>
2026-03-30 17:44:32 +0900

    [Gold III] Title: 줄 세우기, Time: 436 ms, Memory: 45940 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -3,56 +3,57 @@ import java.util.*;
 
 public class Main {
 
-    static List<Integer>[] graph;
-    static int[] indegree;
+   static int N, M;
+   static int[] indegree;
+   static List<Integer>[] graph;
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+   public static void main(String[] args) throws Exception {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+      N = Integer.parseInt(st.nextToken());
+      M = Integer.parseInt(st.nextToken());
 
-        graph = new ArrayList[N + 1];
-        indegree = new int[N + 1];
+      indegree = new int[N + 1];
+      graph = new ArrayList[N + 1];
 
-        for (int i = 1; i <= N; i++) {
-            graph[i] = new ArrayList<>();
-        }
+      for (int i = 1; i <= N; i++) {
+         graph[i] = new ArrayList<>();
+      }
 
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
+      for (int i = 0; i < M; i++) {
+         st = new StringTokenizer(br.readLine());
 
-            int A = Integer.parseInt(st.nextToken());
-            int B = Integer.parseInt(st.nextToken());
+         int A = Integer.parseInt(st.nextToken());
+         int B = Integer.parseInt(st.nextToken());
 
-            graph[A].add(B);
-            indegree[B]++;
-        }
+         graph[A].add(B);
+         indegree[B]++;
+      }
 
-        Queue<Integer> queue = new ArrayDeque<>();
+      Queue<Integer> q = new ArrayDeque<>();
 
-        for (int i = 1; i <= N; i++) {
-            if (indegree[i] == 0) {
-                queue.offer(i);
+      for (int i = 1; i <= N; i++) {
+         if (indegree[i] == 0) {
+            q.offer(i);
+         }
+      }
+
+      StringBuilder sb = new StringBuilder();
+
+      while (!q.isEmpty()) {
+         int cur = q.poll();
+
+         sb.append(cur).append(" ");
+
+         for (int next : graph[cur]) {
+
+            if (--indegree[next] == 0) {
+               q.offer(next);
             }
-        }
+         }
+      }
 
-        StringBuilder sb = new StringBuilder();
-
-        while (!queue.isEmpty()) {
-            int cur = queue.poll();
-            sb.append(cur).append(" ");
-
-            for (Integer next : graph[cur]) {
-                indegree[next]--;
-
-                if (indegree[next] == 0) {
-                    queue.offer(next);
-                }
-            }
-        }
-
-        System.out.println(sb);
-    }
+      System.out.println(sb);
+   }
 }
