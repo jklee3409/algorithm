67a5d92ab88fc0e7585bfc656dfb21a6a1de6ab5
Jong Kyu Lee <92130766+jklee3409@users.noreply.github.com>
2026-03-31 14:44:53 +0900

    [Gold IV] Title: LCS, Time: 128 ms, Memory: 18296 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -1,31 +1,27 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+   public static void main(String[] args) throws Exception {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        char[] str1 = br.readLine().toCharArray();
-        char[] str2 = br.readLine().toCharArray();
+      char[] str1 = br.readLine().toCharArray();
+      char[] str2 = br.readLine().toCharArray();
 
-        int len1 = str1.length;
-        int len2 = str2.length;
+      int len_1 = str1.length;
+      int len_2 = str2.length;
 
-        int[][] dp = new int[len1 + 1][len2 + 1];
+      int[][] dp = new int[len_1 + 1][len_2 + 1];
 
-        for (int i = 1; i <= len1; i++) {
-            for (int j = 1; j <= len2; j++) {
+      for (int i = 1; i <= len_1; i++) {
+         for (int j = 1; j <= len_2; j++) {
 
-                if (str1[i - 1] == str2[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-                }
-            }
-        }
+            if (str1[i - 1] == str2[j - 1]) dp[i][j] = dp[i - 1][j - 1] + 1;
+            else dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+         }
+      }
 
-        System.out.println(dp[len1][len2]);
-    }
+      System.out.println(dp[len_1][len_2]);
+   }
 }
