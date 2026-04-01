1726c4c15a11f229c9d42fa1bd233251d0e6e8c8
Jong Kyu Lee <92130766+jklee3409@users.noreply.github.com>
2026-04-01 22:54:22 +0900

    [Gold III] Title: 텀 프로젝트, Time: 916 ms, Memory: 300684 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
@@ -2,62 +2,56 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int T, N;
-    static int[] graph;
-    static boolean[] visited; // 현재 DFS 경로 방문 여부
-    static boolean[] finished; // 탐색 완료 여부
-    static int count;
-    static StringBuilder sb = new StringBuilder();
+
+    static int N;
+    static int[] pick;
+    static int[] indegree;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        T = Integer.parseInt(br.readLine());
+        StringBuffer sb = new StringBuffer();
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
 
         while (T-- > 0) {
             N = Integer.parseInt(br.readLine());
-            graph = new int[N + 1];
-            visited = new boolean[N + 1];
-            finished = new boolean[N + 1];
-            count = 0;
 
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            pick = new int[N + 1];
+            indegree = new int[N + 1];
+
+            st = new StringTokenizer(br.readLine());
+
             for (int i = 1; i <= N; i++) {
-                graph[i] = Integer.parseInt(st.nextToken());
+                pick[i] = Integer.parseInt(st.nextToken());
+                indegree[pick[i]]++;
             }
 
-            simulate();
+            int notInTeam = 0;
+
+            Queue<Integer> q = new ArrayDeque<>();
+
+            for (int i = 1; i <= N; i++) {
+                if (indegree[i] == 0) {
+                    q.offer(i);
+                }
+            }
+
+            while (!q.isEmpty()) {
+                int cur = q.poll();
+
+                notInTeam++;
+
+                int next = pick[cur];
+
+                if (--indegree[next] == 0) {
+                    q.offer(next);
+                }
+            }
+
+            sb.append(notInTeam).append("\n");
         }
 
         System.out.println(sb);
-    }
-
-    public static void simulate() {
-        for (int i = 1; i <= N; i++) {
-            if (visited[i]) continue;
-            dfs(i);
-        }
-        sb.append(N - count).append("\n");
-    }
-
-    public static void dfs(int current) {
-        visited[current] = true;
-        int next = graph[current];
-
-        if (!visited[next]) {
-            dfs(next);
-
-        } else if (!finished[next]) {
-            // dfs 로 방문헸지만, 탐색이 완료되지 않았음 -> 사이클
-            int temp = next;
-
-            while (true) {
-                count++;
-                temp = graph[temp];
-
-                if (temp == next) break;
-            }
-        }
-
-        finished[current] = true;
     }
 }
