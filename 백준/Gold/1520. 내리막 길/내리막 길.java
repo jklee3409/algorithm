231168cdd77878897d15997d9a157a103d05ab8a
Jong Kyu Lee <92130766+jklee3409@users.noreply.github.com>
2026-04-02 23:43:44 +0900

    [Gold III] Title: 내리막 길, Time: 332 ms, Memory: 35536 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.java
+++ 백준/Gold/1520. 내리막 길/내리막 길.java
@@ -2,25 +2,26 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M;
-    static int[][] map, dp;
 
-    public static void main(String[] args) throws IOException {
+    static int N, M;
+    static int[][] map;
+    static int[][] dp;
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
+
         map = new int[N][M];
         dp = new int[N][M];
-        for (int i = 0; i < N; i++) {
-            Arrays.fill(dp[i], -1);
-        }
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
+                dp[i][j] = -1;
             }
         }
 
@@ -30,17 +31,16 @@ public class Main {
     static int[] dy = {-1, 1, 0, 0};
     static int[] dx = {0, 0, -1, 1};
 
-    // dp[y][x]: (y,x)에서 도착점까지 갈 수 있는 경우의 수
-    static int dfs(int y, int x) {
+    private static int dfs(int y, int x) {
         if (y == N - 1 && x == M - 1) return 1;
 
         if (dp[y][x] != -1) return dp[y][x];
 
         dp[y][x] = 0;
 
-        for (int i = 0; i < 4; i++) {
-            int ny = y + dy[i];
-            int nx = x + dx[i];
+        for (int d = 0; d < 4; d++) {
+            int ny = y + dy[d];
+            int nx = x + dx[d];
 
             if (ny < 0 || nx < 0 || ny >= N || nx >= M) continue;
             if (map[y][x] <= map[ny][nx]) continue;
