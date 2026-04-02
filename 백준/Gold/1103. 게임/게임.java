7ddfec45e539004b03edb50d6451e8de448a2808
Jong Kyu Lee <92130766+jklee3409@users.noreply.github.com>
2026-04-03 00:57:05 +0900

    [Gold II] Title: 게임, Time: 156 ms, Memory: 17536 KB -BaekjoonHub

---
+++ 백준/Gold/1103. 게임/게임.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int N, M;
+    static int[][] board;
+    static int[][] dp;
+    static boolean[][] visited;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        board = new int[N][M];
+        dp = new int[N][M];
+        visited = new boolean[N][M];
+
+        for (int i = 0; i < N; i++) {
+            String input = br.readLine();
+            for (int j = 0; j < M; j++) {
+                char c = input.charAt(j);
+
+                if (c == 'H') board[i][j] = -1;
+                else board[i][j] = c - '0';
+
+                dp[i][j] = -1;
+            }
+        }
+
+        System.out.println(dfs(0, 0));
+    }
+
+    static int[] dy = {-1, 1, 0, 0};
+    static int[] dx = {0, 0, -1, 1};
+
+    private static int dfs(int y, int x) {
+        if (outOfRange(y, x) || isHall(y, x)) return 0;
+
+        if (visited[y][x]) {
+            System.out.println(-1);
+            System.exit(0);
+        }
+
+        if (dp[y][x] != -1) return dp[y][x];
+
+        visited[y][x] = true;
+        dp[y][x] = 0;
+
+        for (int d = 0; d < 4; d++) {
+            int ny = y + dy[d] * board[y][x];
+            int nx = x + dx[d] * board[y][x];
+
+            dp[y][x] = Math.max(dp[y][x], dfs(ny, nx) + 1);
+        }
+
+        visited[y][x] = false;
+        return dp[y][x];
+    }
+
+    private static boolean outOfRange(int y, int x) {
+        return y < 0 || x < 0 || y >= N || x >= M;
+    }
+
+    private static boolean isHall(int y, int x) {
+        return board[y][x] == -1;
+    }
+}
