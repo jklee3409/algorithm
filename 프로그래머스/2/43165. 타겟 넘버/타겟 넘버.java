061f4492154cce08217571b317b405a71d867025
Jong Kyu Lee <92130766+jklee3409@users.noreply.github.com>
2026-05-04 18:50:18 +0900

    [level 2] Title: 타겟 넘버, Time: 7.19 ms, Memory: 82.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,31 +1,19 @@
 import java.util.*;
 
 class Solution {
-    static int count = 0;
     
     public int solution(int[] numbers, int target) {
-        int size = numbers.length;
-        
-        dfs(numbers, 0, 0, size, target);
-        
-        return count;
+        int answer = 0;
+        answer = dfs(numbers, 0, 0, target);
+        return answer;
     }
     
-    private void dfs(int[] arr, int start, int sum, int size, int target) {
-        if (start == size) {
-            if (sum == target) count++;
-            
-            return;
+    private int dfs(int[] arr, int n, int sum, int target) {
+        if (n == arr.length) {
+            if (sum == target) return 1;
+            return 0;
         }
         
-        int temp = sum;
-        
-        sum += arr[start];
-        dfs(arr, start + 1, sum, size, target);
-        
-        sum = temp;
-            
-        sum -= arr[start];
-        dfs(arr, start + 1, sum, size, target);
+        return dfs(arr, n + 1, sum + arr[n], target) + dfs(arr, n + 1, sum - arr[n], target);
     }
 }
