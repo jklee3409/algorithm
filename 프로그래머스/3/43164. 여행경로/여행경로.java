c068acfc9829a814506a1834d948cbdd7e465e43
Jong Kyu Lee <92130766+jklee3409@users.noreply.github.com>
2026-05-10 22:23:50 +0900

    [level 3] Title: 여행경로, Time: 0.90 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Solution {
+    
+    boolean[] visited;
+    String[] answer;
+    
+    public String[] solution(String[][] tickets) {
+        Arrays.sort(tickets, (a, b) -> {
+            if(a[0].equals(b[0])) return a[1].compareTo(b[1]);
+            return a[0].compareTo(b[0]);
+        });
+        
+        visited = new boolean[tickets.length];
+        answer = new String[tickets.length + 1];
+        
+        dfs(tickets, "ICN", 0);
+        
+        return answer;
+    }
+    
+    boolean dfs(String[][] tickets, String current, int depth) {
+        answer[depth] = current;
+        
+        if (depth == tickets.length) return true;
+        
+        for (int i = 0; i < tickets.length; i++) {
+            if (visited[i]) continue;
+            
+            if (tickets[i][0].equals(current)) {
+                visited[i] = true;
+                
+                if(dfs(tickets, tickets[i][1], depth + 1)){
+                    return true;
+                }
+                
+                visited[i] = false;
+            }
+        }
+        
+        return false;
+    }
+}
