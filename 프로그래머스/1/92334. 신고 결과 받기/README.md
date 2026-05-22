7f9757fbe4283c0e5ed96722c163ba58ef39b271
Jong Kyu Lee <92130766+jklee3409@users.noreply.github.com>
2026-05-22 17:31:25 +0900

    [level 1] Title: 신고 결과 받기, Time: 176.17 ms, Memory: 126 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/README.md
+++ 프로그래머스/1/92334. 신고 결과 받기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 신고 결과 받기 - 92334 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92334?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92334) 
 
 ### 성능 요약
 
-메모리: 141 MB, 시간: 170.56 ms
+메모리: 126 MB, 시간: 176.17 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 05월 22일 17:04:54
+2026년 05월 22일 17:31:22
 
 ### 문제 설명
 
--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -1,10 +1,14 @@
 import java.util.*;
 
 class Solution {
-
+    
     public int[] solution(String[] id_list, String[] report, int k) {
         Map<String, Set<String>> reportMap = new HashMap<>();
-        Map<String, Integer> countMap = new HashMap<>();
+        Map<String, Integer> countMap = new HashMap();
+        
+        // 1. 누가 누구 신고했는지 map으로 정리 (중복 제거) 
+        // 2. 신고당한 횟수 map으로 정리
+        // 3. k 이상이면 메일 횟수 + 1
         
         for (int i = 0; i < report.length; i++) {
             String input = report[i];
@@ -19,33 +23,33 @@ class Solution {
         for (int i = 0; i < id_list.length; i++) {
             String reporter = id_list[i];
             Set<String> reportedUserSet = reportMap.get(reporter);
-
+            
             if (reportedUserSet == null || reportedUserSet.isEmpty()) continue;
             
             for (String reportedUser : reportedUserSet) {
                 countMap.put(reportedUser, countMap.getOrDefault(reportedUser, 0) + 1);
             }
         }
-
+        
         int[] answer = new int[id_list.length];
         
         for (int i = 0; i < id_list.length; i++) {
             String reportedUser = id_list[i];
             
             int count = countMap.getOrDefault(reportedUser, 0);
-
+            
             if (k <= count) {
                 for (int j = 0; j < id_list.length; j++) {
                     String reporter = id_list[j];
                     Set<String> reportedUserSet = reportMap.get(reporter);
-                    
+            
                     if (reportedUserSet == null || reportedUserSet.isEmpty()) continue;
                     
                     if (reportedUserSet.contains(reportedUser)) {
                         answer[j]++;
                     }
                 }
-            } 
+            }
         }
         
         return answer;
