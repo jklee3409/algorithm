496fd92a11ff510aaf8f44a952dea2dee3101535
Jong Kyu Lee <92130766+jklee3409@users.noreply.github.com>
2026-05-22 17:04:58 +0900

    [level 1] Title: 신고 결과 받기, Time: 170.56 ms, Memory: 141 MB -BaekjoonHub

---
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+class Solution {
+
+    public int[] solution(String[] id_list, String[] report, int k) {
+        Map<String, Set<String>> reportMap = new HashMap<>();
+        Map<String, Integer> countMap = new HashMap<>();
+        
+        for (int i = 0; i < report.length; i++) {
+            String input = report[i];
+            StringTokenizer st = new StringTokenizer(input);
+            
+            String reporter = st.nextToken();
+            String reportedUser = st.nextToken();
+            
+            reportMap.computeIfAbsent(reporter, x -> new HashSet<>()).add(reportedUser);
+        }
+        
+        for (int i = 0; i < id_list.length; i++) {
+            String reporter = id_list[i];
+            Set<String> reportedUserSet = reportMap.get(reporter);
+
+            if (reportedUserSet == null || reportedUserSet.isEmpty()) continue;
+            
+            for (String reportedUser : reportedUserSet) {
+                countMap.put(reportedUser, countMap.getOrDefault(reportedUser, 0) + 1);
+            }
+        }
+
+        int[] answer = new int[id_list.length];
+        
+        for (int i = 0; i < id_list.length; i++) {
+            String reportedUser = id_list[i];
+            
+            int count = countMap.getOrDefault(reportedUser, 0);
+
+            if (k <= count) {
+                for (int j = 0; j < id_list.length; j++) {
+                    String reporter = id_list[j];
+                    Set<String> reportedUserSet = reportMap.get(reporter);
+                    
+                    if (reportedUserSet == null || reportedUserSet.isEmpty()) continue;
+                    
+                    if (reportedUserSet.contains(reportedUser)) {
+                        answer[j]++;
+                    }
+                }
+            } 
+        }
+        
+        return answer;
+    }
+}
